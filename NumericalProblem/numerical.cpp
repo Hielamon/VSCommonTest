1e3f16d34ee3adce61f8069e6a13d3f8c645e227
邓海林 <borenmicro@outlook.com>
2018-01-23 14:17:25 +0800

    test the Nan property

--- NumericalProblem/numerical.cpp
+++ NumericalProblem/numerical.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[])
 {
 	float nanTest = std::numeric_limits<float>::quiet_NaN();
-	float a = std::numeric_limits<float>::quiet_NaN();
-	if (_isnanf(nanTest))
+	float a = nanTest;
+	if (_isnanf(a))
 	{
 		std::cout << "nanTest is " << nanTest << std::endl;
 	}
